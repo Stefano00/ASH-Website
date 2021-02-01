d5e9a2a2b72e075be27abf25e7ed12cff37fea85
Stefano00 <stefano.marin.quintana@gmail.com>
2021-01-31 21:43:39 -0300

    Inicio de pagina web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
