dffb2af58679c3dd6bb3278014b6470af9b383ed
Stefano Marín <stefano.marin.quintana@gmail.com>
2021-02-03 22:45:10 -0300

    comienzo de tarjetas ver mas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  cardBoolean1: boolean = false;
+  cardBoolean2: boolean = false;
+  cardBoolean3: boolean = false;
+  cardBoolean4: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  card1(){
+    this.cardBoolean1 = true;
+  }
+  card2(){
+    this.cardBoolean2 = true;
+  }
+  card3(){
+    this.cardBoolean3 = true;
+  }
+  card4(){
+    this.cardBoolean4 = true;
   }
 
 }
