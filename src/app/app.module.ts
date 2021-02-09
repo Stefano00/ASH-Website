42fda8b9363d7cd8e37122dc5d3394a9859a5f48
Stefano Marín <stefano.marin.quintana@gmail.com>
2021-02-08 21:44:36 -0300

    despliegue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,8 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// import { MatSliderModule } from '@angular/material/slider';
 import { MapsComponent } from './components/maps/maps.component';
 
-// import { AgmCoreModule } from '@agm/core';
 import { TitleComponent } from './components/title/title.component';
 
 
@@ -27,10 +25,7 @@ import { TitleComponent } from './components/title/title.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule
-    // MatSliderModule,
-    // AgmCoreModule.forRoot({
-    //   apiKey: 'AIzaSyC5mEIW1C_0AxD0TAL9gWCm02OyavgKUDU'
-    // })
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
