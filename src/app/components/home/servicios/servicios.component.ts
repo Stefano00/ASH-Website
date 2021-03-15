55092eb9237ea1d64719a90596f5327c7b535605
smarin <s.marin@amf.cl>
2021-03-14 22:10:17 -0300

    cambio de textos

--- src/app/components/home/servicios/servicios.component.ts
+++ src/app/components/home/servicios/servicios.component.ts
@@ -12,6 +12,9 @@ export class ServiciosComponent implements OnInit {
   cardBoolean2: boolean = false;
   cardBoolean3: boolean = false;
   cardBoolean4: boolean = false;
+  cardBoolean5: boolean = false;
+  cardBoolean6: boolean = false;
+  cardBoolean7: boolean = false;
 
 
   constructor() { }
@@ -25,24 +28,63 @@ export class ServiciosComponent implements OnInit {
     this.cardBoolean2 = false;
     this.cardBoolean3 = false;
     this.cardBoolean4 = false;
+    this.cardBoolean5 = false;
+    this.cardBoolean6 = false;
+    this.cardBoolean7 = false;
   }
   card2(){
     this.cardBoolean1 = false;
     this.cardBoolean2 = true;
     this.cardBoolean3 = false;
     this.cardBoolean4 = false;
+    this.cardBoolean5 = false;
+    this.cardBoolean6 = false;
+    this.cardBoolean7 = false;
   }
   card3(){
     this.cardBoolean1 = false;
     this.cardBoolean2 = false;
     this.cardBoolean3 = true;
     this.cardBoolean4 = false;
+    this.cardBoolean5 = false;
+    this.cardBoolean6 = false;
+    this.cardBoolean7 = false;
   }
   card4(){
     this.cardBoolean1 = false;
     this.cardBoolean2 = false;
     this.cardBoolean3 = false;
     this.cardBoolean4 = true;
+    this.cardBoolean5 = false;
+    this.cardBoolean6 = false;
+    this.cardBoolean7 = false;
+  }
+  card5(){
+    this.cardBoolean1 = false;
+    this.cardBoolean2 = false;
+    this.cardBoolean3 = false;
+    this.cardBoolean4 = false;
+    this.cardBoolean5 = true;
+    this.cardBoolean6 = false;
+    this.cardBoolean7 = false;
+  }
+  card6(){
+    this.cardBoolean1 = false;
+    this.cardBoolean2 = false;
+    this.cardBoolean3 = false;
+    this.cardBoolean4 = false;
+    this.cardBoolean5 = false;
+    this.cardBoolean6 = true;
+    this.cardBoolean7 = false;
+  }
+  card7(){
+    this.cardBoolean1 = false;
+    this.cardBoolean2 = false;
+    this.cardBoolean3 = false;
+    this.cardBoolean4 = false;
+    this.cardBoolean5 = false;
+    this.cardBoolean6 = false;
+    this.cardBoolean7 = true;
   }
 
 }
